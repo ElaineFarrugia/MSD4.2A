cc4f53483ec92310d518c34332d2e2378fdacdc5
ElaineFarrugia <elaine.vassallo@mcast.edu.mt>
2024-10-03 13:41:35 +0200

    introductory example
    introductory example

---
+++ Introd/Introd/Program.cs
@@ -0,0 +1,17 @@
+﻿
+int generatedNumber; //declare variable to hold generated number
+
+Random rand = new Random();    //prepare to generate a random number
+generatedNumber = rand.Next(1, 11); //generate random no. from 1-10
+
+//Console.WriteLine(generatedNumber); //print generated number
+
+Console.WriteLine("I selected a number between 1 and 10. Can you guess it?");
+
+string userInput = Console.ReadLine();  //read user input
+int userInputInt = Convert.ToInt32(userInput); //convert user input to int
+
+if (generatedNumber == userInputInt)
+    Console.WriteLine("You guessed!");
+else
+    Console.WriteLine("Sorry, you did not guess!");
