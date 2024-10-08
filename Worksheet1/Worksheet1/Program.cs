d009296e70aa28d835b635823471cfe7b8134827
Elaine Farrugia <>
2024-10-08 11:33:11 +0200

    Worksheet1

---
+++ Worksheet1/Worksheet1/Program.cs
@@ -0,0 +1,15 @@
+﻿string sequence = "fdsfd76"; //variable declaration and instantiation
+
+Console.WriteLine("Remember this sequence: " + sequence);
+
+Thread.Sleep(5000); //5s delay
+
+Console.Clear();
+
+Console.WriteLine("Type in the sequence:");
+string userInput = Console.ReadLine();
+
+if (userInput == sequence)
+    Console.WriteLine("Correct! You have good memory!");
+else
+    Console.WriteLine("Sorry, the correct sequence was " + sequence);
