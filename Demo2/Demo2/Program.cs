da347f57031e265961d99958d6062b34d2e0b1a3
Elaine Farrugia <>
2024-10-08 12:15:04 +0200

    Demo 2

---
+++ Demo2/Demo2/Program.cs
@@ -0,0 +1,23 @@
+﻿Console.WriteLine("Enter the theory mark:");
+string theoryMark = Console.ReadLine();
+
+Console.WriteLine("Enter the practical mark:");
+string practiceMark = Console.ReadLine();
+
+int theoryMarkInt = Convert.ToInt32(theoryMark); //convert to int
+int practiceMarkInt = Convert.ToInt32(practiceMark);//convert to int
+
+int total = theoryMarkInt + practiceMarkInt; //find total
+
+//check conditions of passing the test
+if ((theoryMarkInt >= 50) && (practiceMarkInt >= 50) && (total >= 125))
+{
+    Console.WriteLine("Congratulations you passed!");    
+}
+else
+{
+    Console.WriteLine("Unfortunately you have failed the test!");
+}
+
+
+
