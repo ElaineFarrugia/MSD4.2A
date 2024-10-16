cdf66dff73cdcbd149c2f01dcb92bea25d00c8f8
Elaine Farrugia <>
2024-10-16 15:29:07 +0200

    Demo4

---
+++ Demo4/Demo4/Program.cs
@@ -0,0 +1,36 @@
+﻿Random generator = new Random();
+char choice = ' ';
+
+do
+{
+    int rand = generator.Next(1, 4);
+
+    char computerChoice = ' ';
+
+    if (rand == 1)
+        computerChoice = 'r';
+    else if (rand == 2)
+        computerChoice = 'p';
+    else
+        computerChoice = 's';
+
+    Console.WriteLine("Computer chose " + computerChoice);
+
+    Console.WriteLine("Choose Rock(r), Paper(p) or Scissors(s)");
+
+    char userInput = Console.ReadKey().KeyChar;
+
+    Console.WriteLine();
+
+    if ((userInput == 'r' && computerChoice == 's')
+        || (userInput == 's' && computerChoice == 'p')
+        || (userInput == 'p' && computerChoice == 'r'))
+        Console.WriteLine("You win!");
+    else if (userInput == computerChoice)
+        Console.WriteLine("It's a tie!");
+    else
+        Console.WriteLine("You Lose!");
+
+    Console.WriteLine("Do you want to play again? (y/n)");
+    choice = Console.ReadKey().KeyChar;
+} while (choice == 'y');
---
+++ LoopsDemo/LoopsDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Console.WriteLine("How many times?");
+string inputStr = Console.ReadLine();
+
+int input = Convert.ToInt32(inputStr);
+
+for (int i = 1; i <= input; i++)
+    Console.WriteLine("Hello "+i);
+
+Console.WriteLine("What multiplication table do you want?");
+string numStr = Console.ReadLine();
+int num = Convert.ToInt32(numStr);
+
+for (int i = 1; i <= 10; i++)
+    Console.WriteLine(num+" * "+i+" = "+(num*i));
+*/
+
+for (int i = 5; i >= 0; i--)
+{
+    Console.WriteLine(i);
+    Thread.Sleep(1000);
+}
