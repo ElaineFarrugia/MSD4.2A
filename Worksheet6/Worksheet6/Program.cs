619b10d843dd8dfedf21c926ee5c2050d46790e2
Elaine Farrugia <>
2024-10-23 15:44:15 +0200

    worksheet 6

---
+++ Worksheet6/Worksheet6/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+
+List<string> names = new List<string>();
+
+while (true)
+{
+    Console.WriteLine("Enter name: ");
+    string input = Console.ReadLine();
+
+    if (input == "stop")
+        break;
+
+    if (names.Contains(input))
+        Console.WriteLine("Duplicate");
+    else
+        names.Add(input);
+}
+
+foreach (string name in names)
+    { Console.WriteLine(name); }
+
+Random generator = new Random();
+int randomNum = generator.Next(0, names.Count);
+
+Console.WriteLine("The 1st winner is: " + names[randomNum]);
+
+names.RemoveAt(randomNum);
+
+int randomNum2 = generator.Next(0, names.Count);
+
+Console.WriteLine("The 2nd winner is: " + names[randomNum2]);
