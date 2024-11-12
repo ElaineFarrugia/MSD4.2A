4ce5b81c0707d6b836c69e4fcb1240f2ffb119ab
Elaine Farrugia <>
2024-11-12 14:19:53 +0100

    Demo 8 - OOP

---
+++ Demo8_OOP/Demo8_OOP/BankAccount.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+
+namespace Demo8_OOP
+{
+    public class BankAccount
+    {
+        //defining the properties
+        public string AccNum;
+        public string HolderName;
+        public string Pin;
+        public double Balance;
+
+        //constructor
+        public BankAccount(string _accNum,
+                           string _HolderName, 
+                           string _Pin,
+                           double _Balance)
+        {
+            AccNum = _accNum;
+            HolderName = _HolderName;
+            Pin = _Pin;
+            Balance = _Balance;
+        }
+
+        public BankAccount() { } //parameter-less
+
+        //behaviour/actions
+        public string GetData()
+        {
+            return "Acc Num: " + AccNum +
+                   " Holder Name: " + HolderName +
+                   " Pin: " + Pin +
+                   " Balance: " + Balance;
+        }
+
+        public void Deposit(double amt)
+        {
+            Balance += amt;
+            //OR
+            //Balance = Balance + amt;
+        }
+
+        public bool Withdraw(double amt)
+        {
+            if (Balance >= amt)
+            {
+                Balance -= amt;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Demo8_OOP/Demo8_OOP/Program.cs
@@ -0,0 +1,31 @@
+﻿using Demo8_OOP;
+
+BankAccount account1 = new BankAccount();
+account1.AccNum = "1";
+account1.HolderName = "Sam";
+account1.Pin = "1234";
+account1.Balance = 500;
+
+
+BankAccount account2 = new BankAccount();
+account2.AccNum = "2";
+account2.HolderName = "Mary";
+account2.Pin = "5678";
+account2.Balance = 1000;
+
+Console.WriteLine(account1.GetData());
+Console.WriteLine(account2.GetData());
+
+account1.Deposit(200);
+Console.WriteLine("After deposit:");
+Console.WriteLine(account1.GetData());
+
+bool result = account1.Withdraw(200);
+if (result == true)
+    Console.WriteLine("Withdraw accepted.");
+else
+    Console.WriteLine("Not enough funds!");
+
+Console.WriteLine(account1.GetData());
+
+BankAccount account3 = new BankAccount("3","Mick","3456",300);
