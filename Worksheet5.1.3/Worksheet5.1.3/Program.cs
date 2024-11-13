c5cec5cda43e7e5dd1289fb66c4d9d1495fd7a95
Elaine Farrugia <>
2024-11-13 15:34:48 +0100

    Worksheet5.1.3 Part 1

---
+++ Worksheet5.1.3/Worksheet5.1.3/Program.cs
@@ -0,0 +1,6 @@
+﻿using Worksheet5._1._3;
+
+Vehicle v1 = new Vehicle("MCT-001", "Toyota", "Avensis",
+                    1500, 7.9, 50);
+
+Console.WriteLine(v1.GetData());
---
+++ Worksheet5.1.3/Worksheet5.1.3/Vehicle.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+
+namespace Worksheet5._1._3
+{
+    public class Vehicle
+    {
+        //variables
+        public string Vin;
+        public string Make;
+        public string Model;
+        public int Weight;
+        public double FuelCons;
+        public double FuelCapacity;
+        public double FuelLevel;
+        
+        public Vehicle() { } //parameter-less
+
+        //parametrised
+        public Vehicle(string _Vin,
+                       string _Make,
+                       string _Model,
+                       int _Weight,
+                       double _FuelCons,
+                       double _FuelCapacity)
+        {
+            Vin = _Vin;
+            Make = _Make;
+            Model = _Model;
+            Weight = _Weight;
+            FuelCons = _FuelCons;
+            FuelCapacity = _FuelCapacity;
+            FuelLevel = _FuelCapacity / 2;
+        }
+
+        public string GetData()
+        {
+            return "Vin: " + Vin +
+                   "\nMake: " + Make +
+                   "\nModel: " + Model +
+                   "\nWeight: " + Weight +
+                   "\nFuel Consumption: " + FuelCons +
+                   "\nFuel Capacity: " + FuelCapacity +
+                   "\nFuel Level: " + FuelLevel;
+        }
+
+        public void FillUpFuelTank()
+        {
+            FuelLevel = FuelCapacity;
+        }
+
+        public bool MakeTrip(string destination)
+        {
+            double RequiredFuel = 0;
+
+            if (destination == "Valletta")
+                RequiredFuel = 8 / 100 * FuelCons;
+
+            else if (destination == "Sliema")
+                RequiredFuel = 10 / 100 * FuelCons;
+
+            else if (destination == "Bugibba")
+                RequiredFuel = 19 / 100 * FuelCons;
+
+            if (FuelLevel > RequiredFuel) //WE HAVE ENOUGH FUEL!
+            {
+                FuelLevel -= RequiredFuel;
+                return true;                                
+            }
+            else //WE DON'T HAVE ENOUGH FUEL
+            {
+                return false;
+            }
+        }
+    }
+}
